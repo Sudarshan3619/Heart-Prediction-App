5e65fa32716b72375c756476fcb345e268d1b3a8
sudarshan <55554449+Sudarshan3619@users.noreply.github.com>
2020-08-11 16:05:54 +0530

    Update heart_app.py

--- heart_app.py
+++ heart_app.py
@@ -99,7 +99,4 @@ def predict_by4():
         
         
 if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000)
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-    #app.run()
+    app.run(debug=True)
