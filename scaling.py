2638585aef1fc8648c47281cf325594f4e07a8ae
sudarshan <55554449+Sudarshan3619@users.noreply.github.com>
2020-08-11 17:50:35 +0530

    Add files via upload

--- scaling.py
+++ scaling.py
@@ -1,13 +1,28 @@
-class scaling:
-    def mean(self,feature):
-        data=pd.read_csv("heart prediction.csv")
-        return data[feature].mean()
-        
-    def sd(self,feature):
-        data=pd.read_csv("heart prediction.csv")
-        return data[feature].std()
-
-
 import pandas as pd
+
+def scaler(self,x,feature):
+    data=pd.read_csv("Data.csv")
+    self.mean=data[feature].mean()
+    self.sd=data[feature].std()
+    return ((x-self.mean)/self.sd)
+
+## Data Pre-Processing 
+def data_preprocessing(data):
+    l=[];
+    for feature,_ in data.items():
+        l.append(float(data[feature]))
+    
+    a=float(data['sysBP'])
+    if a>0:
+        a=np.log(a)
+    l[-1]=a;
+    i=0
+    for feature in ['male','age','cigsPerDay','prevalentStroke','prevalentHyp','diabetes','sysBP']:
+        l[i]=scaler(l[i],feature)
+        i+=1
+    return l;
+
+        
+
 
     
