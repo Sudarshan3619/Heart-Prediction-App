253f45e7e3842496003646c33a0198b3ad939bbb
sudarshan <55554449+Sudarshan3619@users.noreply.github.com>
2020-08-12 08:23:07 +0530

    Update heart_app.py

--- heart_app.py
+++ heart_app.py
@@ -6,7 +6,6 @@ from scaling import data_preprocessing
 
 app=Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -21,7 +20,7 @@ def predict():
         data=request.form
         l=data_preprocessing(data)
         inputs=np.array([l])
-        
+        rfc,lr,abc,knn=pickle.load(open('heart_all.pkl','rb')
         result=rfc.predict(inputs)
         return render_template('output.html',data=result[0])
 
@@ -37,6 +36,7 @@ def predict1():
         inputs=np.array([l])
         result=[0,0,0,0]
         #model=pickle.load(open('heart.pkl','rb'))
+        rfc,lr,abc,knn=pickle.load(open('heart_all.pkl','rb')
         result[0]=rfc.predict(inputs)[0]
         result[1]=lr.predict(inputs)[0]
         result[2]=abc.predict(inputs)[0]
@@ -50,6 +50,5 @@ def predict1():
         
         
 if __name__=='__main__':
-    with open('heart_all.pkl','rb') as f:
-        rfc,lr,abc,knn=pickle.load(f)
+   
     app.run(debug=True)
